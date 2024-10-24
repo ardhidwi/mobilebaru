1156a7c1543e1fbfb0f2a4b5baa23199876b6245
Aonmoy <aonmoy0.0@gmail.com>
2024-10-25 02:12:09 +0600

    feat(Logic): Admin side booking query by date range API integrated

---
+++ lib/src/feature/admin/bookings/res/endpoints.dart
@@ -0,0 +1,7 @@
+import 'package:car_workshop_flutter/src/res/base.dart';
+
+class EndPoints {
+  static const _base = BasePaths.baseUrl;
+  static const bookings = "$_base/bookings";
+  static const bookingsByDateRange = "$_base/bookings/range";
+}
---
+++ lib/src/feature/admin/bookings/res/messages.dart
@@ -0,0 +1,9 @@
+class FailureMessage {
+  static const jsonParse = "JSON parsing failed";
+  static const loginFailed = "Booking Fetch Failed";
+}
+
+class SuccessMessage {
+  static const bookingSuccess = "Bookings Fetched Successfully";
+  static const registerSuccess = "Registration Successful";
+}
