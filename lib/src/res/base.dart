20efbf6f76778c614cf51f5dd04ab55323aa71c5
Aonmoy <aonmoy0.0@gmail.com>
2024-10-25 14:42:37 +0600

    chore: Switch to production server

--- lib/src/res/base.dart
+++ lib/src/res/base.dart
@@ -7,7 +7,7 @@ class BasePaths {
   static const baseAnimationPath = "assets/animations";
   static const baseIconPath = "assets/icons";
   static const basePlaceholderPath = "assets/placeholders";
-  static const baseProdUrl = "http://192.168.0.108:8000/api/v1";
-  static const baseTestUrl = "http://192.168.0.108:8000/api/v1";
+  static const baseProdUrl = "https://car-workshop-fastapi.onrender.com/api/v1";
+  static const baseTestUrl = "https://car-workshop-fastapi.onrender.com/api/v1";
   static const baseUrl = AppConfig.devMode ? baseTestUrl : baseProdUrl;
 }
